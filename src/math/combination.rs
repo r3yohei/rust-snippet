66ab804bf2a8e15d2a2787f03c499d7e48864f69
r3yohei@gmail.com <r3yohei@gmail.com>
2023-07-23 02:53:14 +0900

    nが小さい時のnCrの計算を追加

--- src/math/combination.rs
+++ src/math/combination.rs
@@ -1,11 +1,22 @@
-fn combination(n: usize, r: usize) -> usize {
-    let mut ncr = vec![vec![0; r+1]; n+1];
-    ncr[0][0] = 1;
-    for i in 1..n {
-        ncr[i][0] = 1;
-        for j in 1..=n.min(r) {
-            ncr[i][j] = ncr[i-1][j-1] + ncr[i-1][j];
-        }
+use cargo_snippet::snippet;
+
+/// n <= 200くらいまでの組合せ
+#[snippet("r3yohei_ncr_small")]
+fn nCr_small(n: u128, r: u128) -> u128 {
+    //! オーバーフローしないように，以下の工夫をする
+    //! nCr = n*(n-1)*...*(n-r+1) / 1*2*...*r より，
+    //! n/1 * (n-1)/2 *...* (n-r+1) / r
+    let mut ncr = 1;
+    for i in 1..=r {
+        ncr *= n - i + 1;
+        ncr /= i;
     }
-    ncr[n][r]
+    ncr
+}
+
+#[test]
+fn test_nCr_small() {
+    let n = 16;
+    let r = 11;
+    assert_eq!(nCr_small(n, r), 4368);
 }
