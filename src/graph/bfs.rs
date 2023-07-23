85b7d1562b2663636549961a88858f33119ea9f3
r3yohei@gmail.com <r3yohei@gmail.com>
2023-07-23 20:10:05 +0900

    隣接リスト表現に対する01bfsを追加

--- src/graph/bfs.rs
+++ src/graph/bfs.rs
@@ -18,3 +18,37 @@ fn bfs(edges: &Vec<Vec<usize>>, s: usize) {
         }
     }
 }
+
+/// 隣接リスト表現に対する01BFS
+/// 辺のコストが0or1のグラフに使用する
+#[snippet("r3yohei_01bfs")]
+fn zero_one_bfs(edges: &Vec<Vec<(usize, i64)>>, s: usize) {
+    //! 辺のコストが1ならdequeの右端，0なら左端を繰り返す
+    //! dequeの中身の頂点の暫定最短距離が常に(1,1,2,3,4,4,..)
+    //! のように，左と同じかそれ+1であるように並ぶ
+    //! 暫定距離が短いものからpop_frontしたいのでこうなる
+    //! ダイクストラでheapqを使うのと同じ気持ちだが，dequeなので計算量がO(V+ElogV)からO(V+E)に落ちる
+    let n = edges.len();
+    const INF: i64 = 1_000_000_000;
+    let mut dist = vec![INF; n];
+    dist[s] = 0;
+    let mut deque = VecDeque::new();
+    deque.push_back(s);
+
+    while let Some(frm) = deque.pop_front() {
+        for &(to, cost) in &edges[frm] {
+            let d = dist[frm] + cost;
+            // frmから行くほうが短いなら更新する
+            if d < dist[to] {
+                dist[to] = d;
+                if cost == 1 {
+                    // 辺のコストが1なら，右端に詰める
+                    deque.push_back(to);
+                } else {
+                    // 0なら，左端
+                    deque.push_front(to);
+                }
+            }
+        }
+    }
+}
