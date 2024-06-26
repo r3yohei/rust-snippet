7d83b1d27c59dfe666916a05c233a8f943108ec7
r3yohei <r3yohei@gmail.com>
2024-06-26 18:30:00 +0900

    mod meguru_bisect

--- src/algorithm/meguru_bisect.rs
+++ src/algorithm/meguru_bisect.rs
@@ -1,8 +1,8 @@
 use cargo_snippet::snippet;
 
 #[snippet("r3yohei_is_ok")]
-fn is_ok(mid: i64) -> bool {
-    todo!();
+fn is_ok(mid: isize) -> bool {
+    todo!()
 }
 
 /// めぐる式二分探索(https://aotamasaki.hatenablog.com/entry/meguru_bisect)
@@ -10,8 +10,8 @@ fn is_ok(mid: i64) -> bool {
 /// ok: '取り得る最大の値' + 1
 #[snippet("r3yohei_meguru_bisect")]
 #[snippet(include = "r3yohei_is_ok")]
-fn meguru_bisect(mut ng: i64, mut ok: i64) -> i64 {
-    while (ok - ng).abs() > 1 {
+fn meguru_bisect(mut ng: isize, mut ok: isize) -> usize {
+    while ok.abs_diff(ng) > 1 {
         let mid = (ok + ng) / 2;
         if is_ok(mid) {
             ok = mid;
@@ -19,5 +19,6 @@ fn meguru_bisect(mut ng: i64, mut ok: i64) -> i64 {
             ng = mid;
         }
     }
-    ok
+
+    ok as usize
 }
